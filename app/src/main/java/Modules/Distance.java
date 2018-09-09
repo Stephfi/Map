a75ae510e094e3df59266f5f0ae6d411817ab337
Stephfi <42011823+Stephfi@users.noreply.github.com>
2018-09-09 21:56:12 +0200

    Initial commit

---
+++ app/src/main/java/Modules/Distance.java
@@ -0,0 +1,15 @@
+package Modules;
+
+/**
+ * Created by Mai Thanh Hiep on 4/3/2016.
+ */
+//Distance between start and destination
+public class Distance {
+    public String text;
+    public int value;
+
+    public Distance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/Modules/Duration.java
@@ -0,0 +1,16 @@
+package Modules;
+
+/**
+ * Created by Mai Thanh Hiep on 4/3/2016.
+ */
+
+//Duration between start and destination
+public class Duration {
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
